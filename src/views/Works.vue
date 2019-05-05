caad5deee50cbc6d1ff4c6f360858de78ae3600c
liubin007SSR <50104973+liubin007SSR@users.noreply.github.com>
2019-05-05 23:19:46 +0800

    解决设备高度问题

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,17 +1,56 @@
 <template>
 	<div class="app-works">
-		<mt-header title="个人作品"></mt-header>
+		<header id="header" class="mui-bar mui-bar-transparent">
+			<a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+			<h1 class="mui-title">推荐 | 西安</h1>
+		</header>
+		<div class="works-body" :style="{height:(curHeight-50)+'px'}">
+			<div class="video-box">
+				<video src="http://127.0.0.1:3000/video/01.mp4" controls class="video"></video>
+			</div>
+		</div>
 	</div>
 </template>
 <script>
 	export default{
 		data(){
 			return{
-
+				curHeight:document.documentElement.clientHeight
 			}
+		},
+		methods:{
+			// beforMount(){
+			// 	var h = document.documentElement.clientHeight
+			// 	// this.curHeight = h - heigth
+			// 	console.log(this.curHeight);
+			// }
+		},
+		watch:{
+
 		}
 	}
 </script>
 <style scoped>
-
+.app-works .mui-bar-transparent{
+    background:rgba(0,0,0,0.5);
+}
+.app-works .mui-bar-transparent h1{
+	color: #fff;
+}
+.app-works .video-box{
+	padding: 5rem 0;
+	height: 25rem;
+	background: #000;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+.app-works .video{
+	width:100%;
+}
+.app-works .works-body{
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
 </style>
