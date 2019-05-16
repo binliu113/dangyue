177b719cfdb08a9e5bca36176cd25ecbd2f29d50
tedu <lb18740599813@126.com>
2019-05-16 09:07:29 +0800

    增加视图

---
+++ src/views/Youself.vue
@@ -0,0 +1,246 @@
+<template lang="html">
+    <div class="app-myself">
+        <!-- 加载提示 -->
+        <!-- <div class="Toast" v-if="tCode" :style="tostStyle">
+            <div class="toast-box">
+                <span class="mui-spinner"></span>
+                <p>正在加载中..</p>
+            </div>
+        </div> -->
+        <div class="login-show">
+            <div class="bg-box">
+                <img :src="host+user.user_img" class="bg-img">
+            </div>
+            <div class="my-details">
+                <div class="img-box">
+                    <img :src="host+user.user_img" class="my-img">
+                    <div class="btn-box">
+                        <a href="javascript:;">
+                            <span class="lbtn">天机</span>
+                        </a>
+                    </div>
+                </div>
+                <!-- 用户信息 -->
+                <div class="details-user">
+                    <h4 class="user-name" v-text="user.user_name"></h4>
+                    <p class="user-title">当悦号：{{user.phone}}</p>
+                </div>
+                <div class="icon-box">
+                    <p v-text="user.signature">这个人很懒什么都没留下</p>
+                    <span class="icon-item">西安</span>
+                    <span class="icon-item">小哥哥</span>
+                    <span class="icon-item">喜欢女,爱好女</span>
+                </div>
+                <div class="group">
+                    <span class="group-item"><strong v-text="count">0</strong>获赞</span>
+                    <span class="group-item"><strong>0</strong>关注</span>
+                    <span class="group-item"><strong>0</strong>粉丝</span>
+                </div>
+                <ul class="list-navbar">
+                    <li class="list-item" :class="navCode==true ? 'active': ' ' " @click="showWorks">
+                        <span class="item-child">我的作品</span>
+                    </li>
+                    <li class="list-item" :class="navCode==false ? 'active': ' ' " @click="showLike">
+                        <span class="item-child">我的收藏</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="list-content">
+                <div class="list-works" v-show="navCode">
+                    <div class="works-list">
+                        <!-- 作品类表 -->
+                        <div class="works-item" v-for="item of list">
+                            <router-link to="" class="works-link">
+                                <video :src="host+item.src" class="link-icon">
+                                </video>
+                            </router-link>
+                            <div class="works-icon">
+                                <span class="mui-icon-extra mui-icon-extra-heart-filled"></span>
+                                <span>{{item.like_num}}赞</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="list-keep" v-show="!navCode">
+                    暂无
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            host: this.host,
+            tCode: false,
+            navCode: true,      //navbar控制
+            user:{},
+            count:0,
+            list:[],
+            tostStyle:{
+                //加载提示
+                height: document.documentElement.clientHeight+'px'
+            }
+        }
+    },
+    created(){
+      var url = this.host+'user/details';
+      this.axios.get(url,{
+          params:{
+              uid:1
+          }
+      }).then(res=>{
+          var user = res.data.user;
+          this.user = res.data.user;
+          this.count = res.data.count;
+          this.list = res.data.data;
+          this.tCode = false;
+      })
+    },
+    methods:{
+        showWorks() {
+            this.navCode = true;
+        },
+        showLike() {
+            this.navCode = false;
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+/* 提示样式 */
+.app-myself .Toast{
+    width: 100%;
+    text-align: center;
+    background-image: linear-gradient(91deg, rgb(2, 0, 49) 0px, rgb(109, 51, 83) 140%);
+    justify-content: center;
+    flex-direction: column;
+    display: flex;
+    position: fixed;
+    top: 0;
+}
+.app-myself .toast-box{
+    padding-top: 1rem;
+    margin: 0 auto;
+    width: 10rem;
+    background: rgba(0, 0, 0, .5);
+    border-radius: .5rem;
+    display: inline-block;
+}
+/* 主样式 */
+.app-myself .bg-box{
+    width: 100%;
+    height: 12rem;
+    border-bottom: .1rem solid rgba(0, 0, 0, .5);
+}
+.app-myself .bg-box .bg-img{
+    width: 100%;
+    height:100%;
+}
+.app-myself .my-details{
+    background: rgba(0, 0, 0, .5);
+    padding: 0rem 1rem;
+    padding-top: 6rem;
+    position: relative;
+}
+.app-myself .my-details .img-box{
+    width: 100%;
+    height: 7rem;
+    position: absolute;
+    top: -1.5rem;
+}
+.app-myself .my-details .my-img{
+    width: 7rem;
+    height: 100%;
+    border: .2rem solid rgba(0, 0, 0, .5);
+    border-radius: 50%;
+}
+.app-myself .my-details .btn-box{
+    display: inline-block;
+    position: absolute;
+    top: 3rem;
+    right: 2rem;
+}
+.app-myself .my-details .lbtn{
+    font-size: 17px;
+    color: #fff;
+    background: rgba(102,102,102,.5);
+    border-radius: .2rem;
+    padding: .3rem .7rem;
+}
+.app-myself .my-details .details-user{
+    border-bottom: .05rem solid rgba(102,102,102,.5);
+    margin-bottom: .5rem;
+}
+.app-myself .my-details .user-name{
+    color: #fff;
+    margin: .5rem 0;
+}
+.app-myself .my-details .user-title{
+    font-size: 10px;
+    color: #fff;
+}
+.app-myself .my-details .icon-box .icon-item{
+    font-size: 10px;
+    margin: 0 .2rem;
+    padding: .3rem .5rem;
+    border-radius: .2rem;
+    background: rgba(102,102,102,.5);
+}
+.app-myself .my-details .group{
+    margin: 1rem 0;
+}
+.app-myself .my-details .group-item{
+    margin: .5rem;
+    color: #fff;
+    font-size: 16px;
+}
+.app-myself .list-navbar{
+    padding: 0;
+    margin-bottom: 0;
+    list-style: none;
+    display: flex;
+    justify-content: center;
+}
+.app-myself .list-item{
+    padding: .5rem;
+}
+.app-myself .list-item.active{
+    color: #fff;
+    border-bottom: 1px solid #ffc107;
+}
+.app-myself .list-content{
+    margin-top: .1rem;
+}
+.app-myself  .list-content .works-list{
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+.list-content .works-list .works-item{
+    width: 49.5%;
+    height: 12rem;
+    background: #000;
+    margin-bottom: .1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+}
+.list-content .works-list .works-link,.list-content .works-list .works-link .link-icon{
+    width: 100%;
+    display: block;
+}
+.list-content .works-list .works-icon{
+    color: #fff;
+    font-size: 16px;
+    position: absolute;
+    left: .5rem;
+    bottom: .5rem;
+}
+</style>
